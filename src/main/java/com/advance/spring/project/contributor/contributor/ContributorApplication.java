4d5a3c4eb27f3698df7c84a972cbab5eba6dd464
maccogn <tran.n.cam@gmail.com>
2019-01-14 13:21:39 +0100

    Basics spring project structure Depedencies: -JDBC -SECURITY -WEB

---
+++ src/main/java/com/advance/spring/project/contributor/contributor/ContributorApplication.java
@@ -0,0 +1,14 @@
+package com.advance.spring.project.contributor.contributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContributorApplication.class, args);
+	}
+
+}
+
